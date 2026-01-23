4ae806a28c9251291458218d2cccad7f5e54e30b
Maxwell McNeal <mcnealmaxwell@gmail.com>
2026-01-22 17:20:50 -0800

    Add date validation to ExpenseForm to prevent future dates

--- forms.py
+++ forms.py
@@ -8,9 +8,14 @@ from wtforms import (Form,
 )
 from flask_wtf import FlaskForm
 from wtforms_sqlalchemy.fields import QuerySelectField
+import datetime as dt
+
+def is_not_in_future(form, field):
+    if field.data > dt.date.today():
+        raise validators.ValidationError('Date cannot be in the future')
 
 class ExpenseForm(FlaskForm):
-    date = DateField('Date', [validators.DataRequired()])
+    date = DateField('Date', [validators.DataRequired(), is_not_in_future], default=dt.date.today())
     amount = FloatField('Amount', [validators.DataRequired(), validators.NumberRange(min=0.01)])
     retailer = StringField('Retailer', [validators.DataRequired()])
     description = StringField('Description')
