d6ae0c7b3fb3cb1f310389deafbbec97a3567f2a
Maxwell McNeal <mcnealmaxwell@gmail.com>
2026-01-25 17:55:54 -0800

    Refactored ExpenseForm to use DecimalField for amount and SelectFields for category and payment method

--- forms.py
+++ forms.py
@@ -1,15 +1,17 @@
 from wtforms import (Form, 
                      IntegerField, 
-                     FloatField,
+                     DecimalField,
                      StringField,
                      DateField,
                      SubmitField,
+                     SelectField,
                      validators
 )
 from flask_wtf import FlaskForm
 from wtforms_sqlalchemy.fields import QuerySelectField
 
 from helper import get_todays_date, is_date_in_future
+from constants import EXPENSE_CATEGORIES, PAYMENT_METHOD_CHOICES
 
 def future_validator(form, field):
     if is_date_in_future(field.data):
@@ -17,9 +19,9 @@ def future_validator(form, field):
 
 class ExpenseForm(FlaskForm):
     date = DateField('Date', [validators.DataRequired(), future_validator], default=get_todays_date)
-    amount = FloatField('Amount', [validators.DataRequired(), validators.NumberRange(min=0.01)])
+    amount = DecimalField('Amount', [validators.DataRequired(), validators.NumberRange(min=0.01)])
     retailer = StringField('Retailer', [validators.DataRequired()])
     description = StringField('Description')
-    category = StringField('Category', [validators.DataRequired()])
-    payment_method = StringField('Payment Method', [validators.DataRequired()])
+    category = SelectField('Category', [validators.DataRequired()], choices=EXPENSE_CATEGORIES)
+    payment_method = SelectField('Payment Method', [validators.DataRequired()], choices=PAYMENT_METHOD_CHOICES)
     submit = SubmitField('Submit')
