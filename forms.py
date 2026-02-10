482e249d992fa29aa979f55af66ceefb30374edc
Maxwell McNeal <mcnealmaxwell@gmail.com>
2026-02-09 19:26:47 -0800

    Moved expense cateogories and payment methods to new tables in db

--- forms.py
+++ forms.py
@@ -9,9 +9,9 @@ from wtforms import (Form,
 )
 from flask_wtf import FlaskForm
 from wtforms_sqlalchemy.fields import QuerySelectField
-
+from extensions import db
+from models import ExpenseCategory, PaymentMethod
 from helper import get_todays_date, is_date_in_future
-from constants import EXPENSE_CATEGORIES, PAYMENT_METHOD_CHOICES
 
 def future_validator(form, field):
     if is_date_in_future(field.data):
@@ -22,6 +22,22 @@ class ExpenseForm(FlaskForm):
     amount = DecimalField('Amount', [validators.DataRequired(), validators.NumberRange(min=0.01)])
     retailer = StringField('Retailer', [validators.DataRequired()])
     description = StringField('Description')
-    category = SelectField('Category', [validators.DataRequired()], choices=EXPENSE_CATEGORIES)
-    payment_method = SelectField('Payment Method', [validators.DataRequired()], choices=PAYMENT_METHOD_CHOICES)
+    # category = SelectField('Category', [validators.DataRequired()], choices=EXPENSE_CATEGORIES)
+    # payment_method = SelectField('Payment Method', [validators.DataRequired()], choices=PAYMENT_METHOD_CHOICES)
+    category = QuerySelectField(
+        'Category',
+        [validators.DataRequired()],
+        query_factory=lambda: db.session.execute(db.select(ExpenseCategory).order_by(ExpenseCategory.name)).scalars(),
+        get_label='name',
+        allow_blank=True,
+        blank_text='Select category'
+    )
+    payment_method = QuerySelectField(
+        'Payment Method',
+        [validators.DataRequired()],
+        query_factory=lambda: db.session.execute(db.select(PaymentMethod).order_by(PaymentMethod.id)).scalars(),
+        get_label='name',
+        allow_blank=True,
+        blank_text='Select payment method'
+    )
     submit = SubmitField('Submit')
--- models.py
+++ models.py
@@ -1,13 +1,30 @@
 import datetime as dt
-from sqlalchemy import Integer, Float, String, Date
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, Float, String, Date, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from extensions import db
 
 class Expense(db.Model):
+    __tablename__ = "expenses"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     date: Mapped[dt.date] = mapped_column(Date)
     amount: Mapped[float] = mapped_column(Float)
     retailer: Mapped[str] = mapped_column(String(255))
     description: Mapped[str] = mapped_column(String(255))
-    category: Mapped[str] = mapped_column(String(255))
-    payment_method: Mapped[str] = mapped_column(String(255))
+    category_id: Mapped[int] = mapped_column(ForeignKey("expense_categories.id"))
+    payment_method_id: Mapped[int] = mapped_column(ForeignKey("payment_methods.id"))
+    
+    # Relationships
+    category: Mapped["ExpenseCategory"] = relationship()
+    payment_method: Mapped["PaymentMethod"] = relationship()
+    
+class ExpenseCategory(db.Model):
+    __tablename__ = "expense_categories"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
+    
+class PaymentMethod(db.Model):
+    __tablename__ = "payment_methods"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
